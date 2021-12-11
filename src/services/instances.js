eb12fea33dc8b3cd8b569ecef350d4ac96d53d4f
Stefan Maretić <stefan.maretic@gmail.com>
2021-12-11 13:47:52 +0100

    Add axios instance for api

---
+++ src/services/instances.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const apiBaseUrl = "https://api.themoviedb.org/3";
+const apiKey = "c33b71412633be3e4a413c17428d1624";
+
+export const api = axios.create({
+  baseURL: apiBaseUrl,
+  params: {
+    api_key: apiKey,
+  },
+});
