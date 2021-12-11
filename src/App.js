ca2047baff6ffe0f82ab1488a44a27c6e7540e22
Aleksandar J <74408889+alex-aestic@users.noreply.github.com>
2021-12-11 16:13:44 +0100

    Layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home";
 import { QueryClient, QueryClientProvider } from "react-query";
-
+import Layout from "./components/layout";
 const queryClient = new QueryClient();
 
 function App() {
@@ -11,10 +11,12 @@ function App() {
     <ChakraProvider>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="about" element={<h1>About</h1>} />
-          </Routes>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="about" element={<h1>About</h1>} />
+            </Routes>
+          </Layout>
         </BrowserRouter>
       </QueryClientProvider>
     </ChakraProvider>
---
+++ src/components/header.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+
+export function Header() {
+    return <h1> This is Header</h1>;
+}
