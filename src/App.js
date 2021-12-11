cb440ee57928b3a0ca46af6a9fab948783babb19
Aleksandar J <74408889+alex-aestic@users.noreply.github.com>
2021-12-11 16:58:59 +0100

    removing layout from router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home";
 import { QueryClient, QueryClientProvider } from "react-query";
-import Layout from "./components/layout";
 const queryClient = new QueryClient();
 
 function App() {
@@ -11,12 +10,10 @@ function App() {
     <ChakraProvider>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="about" element={<h1>About</h1>} />
-            </Routes>
-          </Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<h1>About</h1>} />
+          </Routes>
         </BrowserRouter>
       </QueryClientProvider>
     </ChakraProvider>
