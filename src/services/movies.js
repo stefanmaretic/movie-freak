c25c70fdbb0df26f602385c86043c9d080178521
Aleksandar J <74408889+alex-aestic@users.noreply.github.com>
2021-12-15 21:29:15 +0100

    Carousel files update

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,4 +1,3 @@
 import { api } from "./instances";
 
 export const getPopularMovies = async () => await api.get("movie/popular");
-export const getTopTvShow = async () => await api.get("tv/popular");
