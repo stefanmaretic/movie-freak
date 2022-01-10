ab416a4416c44a4d5ca72db1a6394dfa15ba3865
Aleksandar J <74408889+alex-aestic@users.noreply.github.com>
2022-01-10 19:45:33 +0100

    Fix tv-shows merge conflict

--- src/services/tv-shows.js
+++ src/services/tv-shows.js
@@ -1,7 +1,4 @@
 import { api } from "./instances";
 
 export const getTopTvShows = async () => await api.get("tv/popular");
-<<<<<<< HEAD
 export const getAirTvShows = async () => await api.get("tv/airing_today");
-=======
->>>>>>> main
