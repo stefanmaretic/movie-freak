722d39f0bc5957200b17687e768f9da29a047cb3
Aleksandar J <74408889+alex-aestic@users.noreply.github.com>
2022-01-24 14:24:29 +0100

    https

--- src/services/instances.js
+++ src/services/instances.js
@@ -2,9 +2,9 @@ import axios from "axios";
 export const apiBaseUrl = "https://api.themoviedb.org/3";
 const apiKey = "c33b71412633be3e4a413c17428d1624";
 
-export const baseImageUrl = "http://image.tmdb.org/t/p/w500/";
-export const baseProfileImg = "http://image.tmdb.org/t/p/w185/";
-export const baseActorImg = "http://image.tmdb.org/t/p/h632/";
+export const baseImageUrl = "https://image.tmdb.org/t/p/w500/";
+export const baseProfileImg = "https://image.tmdb.org/t/p/w185/";
+export const baseActorImg = "https://image.tmdb.org/t/p/h632/";
 export const baseVideoUrl =
   "https://api.themoviedb.org/3/movie/297762?api_key=###&append_to_response=videos";
 export const api = axios.create({
