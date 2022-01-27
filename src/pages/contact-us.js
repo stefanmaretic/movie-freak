292f23b2dc3d631791596de0751898b7a659d38a
Stefan Maretić <stefan.maretic@gmail.com>
2022-01-27 11:24:41 +0100

    Use default name for named import

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -1,6 +1,6 @@
 import { Flex, Text } from "@chakra-ui/react";
 import { Layout } from "../components/layout";
-import { ContactForm as Form } from "../components/contact-form";
+import { ContactForm } from "../components/contact-form";
 
 export default function ContactUs() {
   return (
@@ -9,8 +9,7 @@ export default function ContactUs() {
         <Text align="center" fontSize="4xl" fontWeight="bold" mt="3rem">
           Contact Us
         </Text>
-
-        <Form />
+        <ContactForm />
       </Flex>
     </Layout>
   );
